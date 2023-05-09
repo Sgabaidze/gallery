52eedea2f04b686965994b4677105b83e92d9750
Sgabaidze <acyber345@gmail.com>
2023-05-09 20:19:57 +0400

    first commit

---
+++ gallery.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const galleryImages = document.querySelectorAll('.gallery-image');
+    const modal = document.getElementById('modal');
+    const modalImage = document.querySelector('.modal-image');
+    const closeButton = document.querySelector('.close');
+    const nextButton = document.querySelector('.nxt-btn');
+    const prevButton = document.querySelector('.prev-btn');
+
+    let currentImageIndex ;
+
+        galleryImages.forEach( (image, index) => {
+            image.addEventListener('click', () => {
+                modal.style.display = "block";
+                modalImage.src = image.src;
+                currentImageIndex = index;
+            });
+        });
+
+        closeButton.addEventListener('click', () => {
+            modal.style.display="none";
+        })
+
+       function NextImage(){
+            currentImageIndex++;
+
+            if(currentImageIndex >= galleryImages.length){
+                currentImageIndex = 0;
+            }
+
+            modalImage.src = galleryImages[currentImageIndex].src;
+            console.log(currentImageIndex);
+       } 
+
+       function prevImage(){
+            currentImageIndex--;
+
+            if(currentImageIndex < 0){
+                currentImageIndex = galleryImages.length - 1;
+            }
+
+            modalImage.src = galleryImages[currentImageIndex].src;
+            console.log(currentImageIndex)
+        } 
+
+        nextButton.addEventListener('click', NextImage);
+        prevButton.addEventListener('click', prevImage);
+
+});
